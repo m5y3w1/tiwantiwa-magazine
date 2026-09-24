b98f159885f193e3a46d9c172bc9ee067cffe638
Tiwantiwa Editorial Board <admin@tiwantiwa.com>
2026-09-24 20:31:45 +0100

    refactor: optimize favicon geometry and scaling for 32x32 display

--- scripts/generate_favicon.mjs
+++ scripts/generate_favicon.mjs
@@ -2,22 +2,16 @@ import fs from 'fs';
 import path from 'path';
 
 const PUBLIC_DIR = path.join('/home/earth/digital-magazine', 'public');
-if (!fs.existsSync(PUBLIC_DIR)) fs.mkdirSync(PUBLIC_DIR, { recursive: true });
 
-const faviconSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" width="512" height="512">
-  <!-- Solid background -->
-  <rect width="512" height="512" fill="#09090b" />
+const faviconSvg = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" width="64" height="64">
+  <rect width="64" height="64" rx="12" fill="#09090b" />
   
-  <!-- Stylized 'TW' Monogram in Emerald Green and Gold -->
-  <g transform="translate(64, 96)">
-    <!-- 'T' Component -->
-    <path d="M 30 20 L 210 20 L 210 90 L 140 90 L 140 320 L 100 320 L 100 90 L 30 90 Z" fill="#34d399" />
-    
-    <!-- 'W' Component -->
-    <path d="M 230 20 L 270 20 L 310 200 L 350 20 L 390 20 L 430 200 L 470 20 L 510 20 L 450 320 L 405 320 L 370 150 L 335 320 L 295 320 Z" fill="#fbbf24" />
-  </g>
+  <!-- T in Emerald Green -->
+  <path d="M 12 16 L 36 16 L 36 24 L 28 24 L 28 48 L 20 48 L 20 24 L 12 24 Z" fill="#34d399" />
+  
+  <!-- W in Bright Gold -->
+  <path d="M 38 16 L 43 16 L 46 36 L 49 16 L 54 16 L 57 36 L 60 16 L 64 16 L 58 48 L 54 48 L 51 30 L 48 48 L 44 48 Z" fill="#fbbf24" transform="translate(-24, 0)" />
 </svg>`;
 
-const faviconPath = path.join(PUBLIC_DIR, 'favicon.svg');
-fs.writeFileSync(faviconPath, faviconSvg, 'utf8');
-console.log(`[+] Favicon generated successfully at ${faviconPath}`);
+fs.writeFileSync(path.join(PUBLIC_DIR, 'favicon.svg'), faviconSvg, 'utf8');
+console.log('[+] Favicon optimized for 32x32 legibility.');
